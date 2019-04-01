44c372cd3efce2b66b4d029f30bd28564efca1e7
Yocum, Chris <chryo12@yahoo.com>
2019-04-01 17:10:06 -0400

    Add user pings for Discord notifications

--- docker_container_healthchecks.sh
+++ docker_container_healthchecks.sh
@@ -6,7 +6,12 @@
 # Define some variables
 tempDir='/tmp/'
 containerNamesFile="${tempDir}container_names.txt"
+# Your webhook URL for the Discord channel you want alerts sent to
 discordWebhookURL=''
+# Your Discord numeric user ID
+# To find your user ID just type \@<username> or \@<role>, like so \@username#1337
+# It will look something like <@123492578063015834> and you NEED the exclamation point like below
+discordUserID='<@!123492578063015834>'
 
 # Function to create list of Docker containers
 create_containers_list() {
@@ -20,15 +25,15 @@ check_containers() {
     if [ "${containerStatus}" = 'running' ];then
       :
     elif [ "${containerStatus}" = 'exited' ];then
-      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "The '"${container}"' container is currently stopped!"}' "${discordWebhookURL}"
+      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "'"${discordUserID}"' The '"${container}"' container is currently stopped!"}' "${discordWebhookURL}"
     elif [ "${containerStatus}" = 'dead' ];then
-      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "The '"${container}"' container is currently dead!"}' "${discordWebhookURL}"
+      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "'"${discordUserID}"'The '"${container}"' container is currently dead!"}' "${discordWebhookURL}"
     elif [ "${containerStatus}" = 'restarting' ];then
-      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "The '"${container}"' container is currently restarting!"}' "${discordWebhookURL}"
+      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "'"${discordUserID}"'The '"${container}"' container is currently restarting!"}' "${discordWebhookURL}"
     elif [ "${containerStatus}" = 'paused' ];then
-      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "The '"${container}"' container is currently paused!"}' "${discordWebhookURL}"
+      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "'"${discordUserID}"'The '"${container}"' container is currently paused!"}' "${discordWebhookURL}"
     else
-      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "The '"${container}"' container currently has an unknown status!"}' "${discordWebhookURL}"
+      curl -s -H "Content-Type: application/json" -X POST -d '{"content": "'"${discordUserID}"'The '"${container}"' container currently has an unknown status!"}' "${discordWebhookURL}"
     fi
   done < <(cat "${containerNamesFile}")
 }
