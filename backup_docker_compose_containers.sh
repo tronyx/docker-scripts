ba31aaf1c3f00343e689ef9856eef1371400bda1
Chris Yocum <chryo12@yahoo.com>
2019-07-19 01:23:13 -0400

    Overhaul
    Setup options to backup, update, or both

--- backup_docker_compose_containers.sh
+++ backup_docker_compose_containers.sh
@@ -20,6 +20,105 @@ days=$(( ( $(date '+%s') - $(date -d '2 months ago' '+%s') ) / 86400 ))
 domain='domain.com'
 # Define your SMS e-mail address (AT&T as an example)
 smsAddress='5551235555@txt.att.net'
+# Arguments
+readonly args=("$@")
+# Colors
+readonly grn='\e[32m'
+readonly red='\e[31m'
+readonly ylw='\e[33m'
+readonly lorg='\e[38;5;130m'
+readonly endColor='\e[0m'
+
+# Define usage and script options
+usage() {
+    cat <<- EOF
+
+  Usage: $(echo -e "${lorg}$0${endColor}") $(echo -e "${grn}"-[OPTION]"${endColor}")
+
+  $(echo -e "${grn}"-b/--backup"${endColor}""${endColor}")      Backup all Docker containers.
+  $(echo -e "${grn}"-u/--update"${endColor}")      Update all Docker containers.
+  $(echo -e "${grn}"-a/--all"${endColor}")         Backup and update all Docker containers.
+  $(echo -e "${grn}"-h/--help"${endColor}")        Display this usage dialog.
+
+EOF
+
+}
+
+# Define script options
+cmdline() {
+    local arg=
+    local local_args
+    local OPTERR=0
+    for arg; do
+        local delim=""
+        case "${arg}" in
+            # Translate --gnu-long-options to -g (short options)
+            --backup) local_args="${local_args}-b " ;;
+            --update) local_args="${local_args}-u " ;;
+            --all) local_args="${local_args}-a " ;;
+            --help) local_args="${local_args}-h " ;;
+            # Pass through anything else
+            *)
+                [[ ${arg:0:1} == "-" ]] || delim='"'
+                local_args="${local_args:-}${delim}${arg}${delim} "
+                ;;
+        esac
+    done
+
+    # Reset the positional parameters to the short options
+    eval set -- "${local_args:-}"
+
+    while getopts "hbua" OPTION; do
+        case "$OPTION" in
+            b)
+                backup=true
+                ;;
+            u)
+                update=true
+                ;;
+            a)
+                all=true
+                ;;
+            h)
+                usage
+                exit
+                ;;
+            *)
+                echo -e "${red}You are specifying a non-existent option!${endColor}"
+                usage
+                exit
+                ;;
+        esac
+    done
+    return 0
+}
+
+# Script Information
+get_scriptname() {
+    local source
+    local dir
+    source="${BASH_SOURCE[0]}"
+    while [[ -L ${source} ]]; do
+        dir="$(cd -P "$(dirname "${source}")" > /dev/null && pwd)"
+        source="$(readlink "${source}")"
+        [[ ${source} != /* ]] && source="${dir}/${source}"
+    done
+    echo "${source}"
+}
+
+readonly scriptname="$(get_scriptname)"
+readonly scriptpath="$(cd -P "$(dirname "${scriptname}")" > /dev/null && pwd)"
+
+# Check whether or not user is root or used sudo
+root_check() {
+    if [[ ${EUID} -ne 0 ]]; then
+        echo -e "${red}You didn't run the script as root!${endColor}"
+        echo -e "${red}Doing it for you now...${endColor}"
+        echo ''
+        sudo bash "${scriptname:-}" "${args[@]:-}"
+        exit
+    fi
+}
 
 # Update Docker images
 update_images() {
@@ -67,13 +166,29 @@ cleanup(){
 }
 
 main(){
-  update_images
-  create_containers_list
-  compose_down
-  backup
-  compose_up
-  domain_check
-  cleanup
+  root_check
+  if [ "${backup}" = 'true' ]; then
+    create_containers_list
+    compose_down
+    backup
+    compose_up
+    domain_check
+    cleanup
+  elif [ "${update}" = 'true' ]; then
+    update_images
+    create_containers_list
+    compose_down
+    compose_up
+    domain_check
+    cleanup
+  elif [ "${all}" = 'true' ]; then
+    create_containers_list
+    compose_down
+    backup
+    compose_up
+    domain_check
+    cleanup
+  fi
 }
 
 main
