4cf439035d0e55fc5d17321570d64e60fde7bf36
Chris Yocum <chryo12@yahoo.com>
2019-07-19 02:39:52 -0400

    shftm formatting

--- backup_docker_compose_containers.sh
+++ backup_docker_compose_containers.sh
@@ -122,74 +122,74 @@ root_check() {
 
 # Update Docker images
 update_images() {
-  /usr/local/bin/docker-compose -f "${composeFile}" pull -q
+    /usr/local/bin/docker-compose -f "${composeFile}" pull -q
 }
 
 # Create list of container names
 create_containers_list() {
-  docker ps --format '{{.Names}}' |sort > "${containerNamesFile}"
+    docker ps --format '{{.Names}}' |sort > "${containerNamesFile}"
 }
 
 # Take down containers and networks
 compose_down() {
-  /usr/local/bin/docker-compose -f "${composeFile}" down
+    /usr/local/bin/docker-compose -f "${composeFile}" down
 }
 
 # Loop through all containers to backup appdata dirs
 backup() {
-  while IFS= read -r CONTAINER; do
-    tar czf "${backupDirectory}""${CONTAINER}"-"${today}".tar.gz "${appdataDirectory}""${CONTAINER}"/
-  done < <(cat "${containerNamesFile}")
+    while IFS= read -r CONTAINER; do
+        tar czf "${backupDirectory}""${CONTAINER}"-"${today}".tar.gz "${appdataDirectory}""${CONTAINER}"/
+    done < <(cat "${containerNamesFile}")
 }
 
 # Start containers and sleep to make sure they have time to startup
 compose_up() {
-  /usr/local/bin/docker-compose -f "${composeFile}" up -d --no-color
-  sleep 120
+    /usr/local/bin/docker-compose -f "${composeFile}" up -d --no-color
+    sleep 120
 }
 
 # Unpause UptimeRobot monitors if domain status is 200, otherwise leave them paused and send an SMS
 domain_check(){
-  domainStatus=$(curl -sI https://"${domain}" |grep -i http/ |awk '{print $2}')
-  domainCurl=$(curl -sI https://"${domain}" |head -2)
-  if [ "${domainStatus}" == 200 ]; then
-    :
-  else
-    echo "${domainCurl}" |mutt -s "${domain} is still down after weekly backup!" "${smsAddress}"
-  fi
+    domainStatus=$(curl -sI https://"${domain}" |grep -i http/ |awk '{print $2}')
+    domainCurl=$(curl -sI https://"${domain}" |head -2)
+    if [ "${domainStatus}" == 200 ]; then
+        :
+    else
+        echo "${domainCurl}" |mutt -s "${domain} is still down after weekly backup!" "${smsAddress}"
+    fi
 }
 
 # Cleanup backups older than two months and perform docker prune
 cleanup(){
-  find "${backupDirectory}"*.tar.gz -mtime +"${days}" -type f -delete
-  docker system prune -f -a --volumes
+    find "${backupDirectory}"*.tar.gz -mtime +"${days}" -type f -delete
+    docker system prune -f -a --volumes
 }
 
 main(){
-  root_check
-  cmdline "${args[@]:-}"
-  if [ "${backup}" = 'true' ]; then
-    create_containers_list
-    compose_down
-    backup
-    compose_up
-    domain_check
-    cleanup
-  elif [ "${update}" = 'true' ]; then
-    update_images
-    create_containers_list
-    compose_down
-    compose_up
-    domain_check
-    cleanup
-  elif [ "${all}" = 'true' ]; then
-    create_containers_list
-    compose_down
-    backup
-    compose_up
-    domain_check
-    cleanup
-  fi
+    root_check
+    cmdline "${args[@]:-}"
+    if [ "${backup}" = 'true' ]; then
+        create_containers_list
+        compose_down
+        backup
+        compose_up
+        domain_check
+        cleanup
+    elif [ "${update}" = 'true' ]; then
+        update_images
+        create_containers_list
+        compose_down
+        compose_up
+        domain_check
+        cleanup
+    elif [ "${all}" = 'true' ]; then
+        create_containers_list
+        compose_down
+        backup
+        compose_up
+        domain_check
+        cleanup
+    fi
 }
 
 main
